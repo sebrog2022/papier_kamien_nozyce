de2e966dd59a96eb913c87d49654a9a441227cca
Sebastian Rogiński <sebastian.roginski@onet.pl>
2022-10-24 09:39:55 +0200

    zmiany_nie działa wynik

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const rock = document.querySelector("#rock");
+
+const scrissors = document.querySelector("#scrissors");
+
+const paper = document.querySelector("#paper");
+
+
+
+rock.addEventListener("click", function(){
+const randomMove = createRandomMove();
+displayMoves(this.id, randomMove);
+displayResult(this.id, randomMove);
+})
+
+scrissors.addEventListener("click", function(){
+const randomMove = createRandomMove();
+displayMoves(this.id, randomMove);
+displayResult(this.id, randomMove);
+
+})
+
+paper.addEventListener("click", function(){
+const randomMove = createRandomMove();
+displayMoves(this.id, randomMove);
+displayResult(this.id, randomMove);
+
+})
+
